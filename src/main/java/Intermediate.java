00677e726577a128d8630fd25f48a3deca218e2e
Gadenko <gadenko@gmx.net>
2022-03-24 18:27:34 +0100

    first commit

---
+++ src/main/java/Intermediate.java
@@ -0,0 +1,22 @@
+public class Intermediate {
+
+    public static void main(String[] args) {
+
+        String[] arrays = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"};
+
+        giveMeAnArray(arrays);
+
+    }
+    public static void giveMeAnArray(String[] arrays){
+        System.out.println(arrays[0]);
+        System.out.println(arrays[1]);
+        System.out.println(arrays[2]);
+        System.out.println(arrays[3]);
+        System.out.println(arrays[4]);
+        System.out.println(arrays[5]);
+        System.out.println(arrays[6]);
+        System.out.println(arrays[7]);
+        System.out.println(arrays[8]);
+        System.out.println(arrays[9]);
+    }
+}
